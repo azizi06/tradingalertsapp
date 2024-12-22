b010e94485da3db3d345bf48fb4e346016a4e9d1
AZIZImohammed <artazizi06@gmail.com>
2024-12-22 20:17:06 +0100

    Adding A cart

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stocksalertapp/components/my_bottomAppBar.dart';
+import 'package:stocksalertapp/components/my_stockSquareCard.dart';
 
 class MyExplorePage extends StatefulWidget {
   const MyExplorePage({super.key});
@@ -11,11 +12,29 @@ class MyExplorePage extends StatefulWidget {
 class _MyExplorePageState extends State<MyExplorePage> {
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      bottomNavigationBar: MyBottomAppBar(),
-      body: Center(child: Text("Explore"),),
-
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(preferredSize: Size.fromHeight(100),child: SizedBox(height: 100,),),
+        bottomNavigationBar: MyBottomAppBar(),
+        body: Column(
+            children: [
+              Container( height: 100.0,  child: Center(child: Text('Header Section')), ),
+              Expanded(
+                //height: 400,
+                  child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, // Number of columns in the grid
+                        crossAxisSpacing: 10.0, // Spacing between columns
+                        mainAxisSpacing: 10.0, // Spacing between rows
+                        childAspectRatio: 3 / 2, // Aspect ratio of each item
+                      ),
+                      itemBuilder: (context, index) {
+                        return MyStockSquareCard();
+                      }))
+            ],
+          
+        ),
+      ),
     );
   }
 }
